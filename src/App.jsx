2a9c2733f1524be051f2ddd171a16e4c401c363c
clausaji <c.sanchezjimenez@qquest.nl>
2022-12-02 10:17:54 +0100

    The information is not hardcoded in the component but passed in the html of the App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Header from './Components/Header'
 import Navbar from './Components/Navbar'
 import Card from './Components/Card'
+import zaferes from "./assets/zaferes.png"
 
 function App() {
   return (
@@ -11,9 +12,33 @@ function App() {
       <Navbar />
       <Header />
       <div className='cardDiv'>
-        <Card />
-        <Card />
-        <Card />
+        <Card 
+          img={zaferes} 
+          statusButton="SOLD OUT" 
+          grade={5.0} 
+          nOfVotes={6} 
+          country="USA"
+          description="Life lessons with Katie Zaferes"
+          price={168}
+        />
+        <Card 
+          img={zaferes} 
+          statusButton="SOLD OUT" 
+          grade={5.0} 
+          nOfVotes={6} 
+          country="Spain"
+          description="Life lessons with Katie Zaferes"
+          price={168}
+        />
+        <Card 
+          img={zaferes} 
+          statusButton="SOLD OUT" 
+          grade={5.0} 
+          nOfVotes={6} 
+          country="USA"
+          description="Life lessons with Katie Zaferes"
+          price={168}
+        />        
       </div>
     </div>
   )
