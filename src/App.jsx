29ecf6f26ece8a5d7e7d21616378f2385e509f9b
clausaji <c.sanchezjimenez@qquest.nl>
2022-12-02 13:59:24 +0100

    The info is parsed from a js database with .map. For that the local images are moved to Public

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './Components/Header'
 import Navbar from './Components/Navbar'
 import Card from './Components/Card'
-import zaferes from "./assets/zaferes.png"
+import data from "./data"
 
 function App() {
+  const cardElement = data.map(
+    (card)=>(
+      <Card 
+        key={card.id}
+        id={card.id}
+        img={card.coverImg} 
+        statusButton={card.status} 
+        grade={card.stats.rating} 
+        nOfVotes={card.stats.reviewCount} 
+        country={card.country}
+        title={card.title}
+        price={card.price}
+      />
+    )
+  )
+  
   return (
     <div className="App">
       <Navbar />
       <Header />
       <div className='cardDiv'>
-        <Card 
-          img={zaferes} 
-          statusButton="SOLD OUT" 
-          grade={5.0} 
-          nOfVotes={6} 
-          country="USA"
-          description="Life lessons with Katie Zaferes"
-          price={168}
-        />
-        <Card 
-          img={zaferes} 
-          statusButton="SOLD OUT" 
-          grade={5.0} 
-          nOfVotes={6} 
-          country="Spain"
-          description="Life lessons with Katie Zaferes"
-          price={168}
-        />
-        <Card 
-          img={zaferes} 
-          statusButton="SOLD OUT" 
-          grade={5.0} 
-          nOfVotes={6} 
-          country="USA"
-          description="Life lessons with Katie Zaferes"
-          price={168}
-        />        
+        {cardElement}
       </div>
     </div>
   )
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,16 +4,16 @@ import star from "../assets/star.png"
 
 export default function Card(props) {
     return(
-        <div className="card">
+        <div className="card" id={props.id}>
             <div className="card--imgButtonDiv">
-                <img src={props.img} alt="" className="card--img"/>
+                <img src={`/${props.img}`} alt="" className="card--img"/>
                 <button className="card--stateButton">{props.statusButton}</button>
             </div>
             <div className="card--textDiv">
                 <img src={star} className="star"/>
                 <span className="card--grade">{props.grade.toFixed(1)}</span>
                 <span className="gray"> ({props.nOfVotes})•{props.country}</span>
-                <p className="card--description">{props.description}</p>
+                <p className="card--description">{props.title}</p>
                 <p className="card--price"><span className="bold">From €{props.price}</span> / person</p>
             </div>
         </div>
